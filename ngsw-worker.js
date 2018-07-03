93620e60c7dfd7807288047e258667bd830d2d97
anshul goyal <anshulgoel151999@gmail.com>
2018-07-03 10:49:29 +0530

    kk

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1849,6 +1849,13 @@ class Driver {
         this.scope.addEventListener('fetch', (event) => this.onFetch(event));
         this.scope.addEventListener('message', (event) => this.onMessage(event));
         this.scope.addEventListener('push', (event) => this.onPush(event));
+        this.scope.addEventListener('notificationclick',(event)=>{
+        event.notification.close();
+        console.log('hi')
+        if(clients.openWindow&&event.notification.data.url){
+          event.waitUntil(clients.openWindow(event.notification.data.url))
+        }
+        })
         // The debugger generates debug pages in response to debugging requests.
         this.debugger = new DebugHandler(this, this.adapter);
         // The IdleScheduler will execute idle tasks after a given delay.
